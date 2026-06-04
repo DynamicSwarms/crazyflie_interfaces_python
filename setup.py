3fdce16c40545222ce3bf7ba0c439319afa9a2ab
vinzenzm <vinzenz@malke.info>
2026-06-04 23:38:44 +0200

    fixed lyrical compile

--- setup.py
+++ setup.py
@@ -17,7 +17,11 @@ setup(
     maintainer_email='winni@todo.todo',
     description='TODO: Package description',
     license='TODO: License declaration',
-    tests_require=['pytest'],
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
     entry_points={
         'console_scripts': [
         ],
